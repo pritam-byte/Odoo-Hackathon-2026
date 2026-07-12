064dff095b19542205d34b20d2ee2540128b819d
Farish Saim <saimsarkar225@gmail.com>
2026-07-12 11:37:52 +0530

    setting-up-authentication-authorization

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+//const { errorHandler } = require("./middleware/error.middleware.js");
+
 const app = express();
 
 app.use(
@@ -12,13 +14,16 @@ app.use(
 );
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.get("/api/health", (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     success: true,
     message: "TransitOps backend is running",
   });
 });
+
+//app.use(errorHandler);
 
 module.exports = app;
