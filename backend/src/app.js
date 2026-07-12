f2b1077afbe3d35597ceceb0fb4aab88dd55c63a
Farish Saim <saimsarkar225@gmail.com>
2026-07-12 14:59:23 +0530

    resolving-routing-errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,6 +24,10 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log(`[REQUEST] ${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.get("/api/health", (req, res) => {
   return res.status(200).json({
